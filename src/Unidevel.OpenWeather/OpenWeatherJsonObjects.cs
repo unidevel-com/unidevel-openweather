5ed265881a50fcf2c48fa8ae46ac93cb52228add
Grzegorz Strzałkowski <grzegorz.strzalkowski@live.com>
2019-11-24 11:24:39 +0100

    fixed weatherforecastitem was missing weather property

--- src/Unidevel.OpenWeather/OpenWeatherJsonObjects.cs
+++ src/Unidevel.OpenWeather/OpenWeatherJsonObjects.cs
@@ -100,6 +100,7 @@ namespace Unidevel.OpenWeather
         [JsonProperty("main")] public Main Main { get; set; }
         [JsonProperty("wind")] public Wind Wind { get; set; }
         [JsonProperty("clouds")] public Clouds Clouds { get; set; }
+        [JsonProperty("weather")] public Weather[] Weather { get; set; }
         [JsonProperty("rain", NullValueHandling = NullValueHandling.Ignore)] public Rain Rain { get; set; }
         [JsonProperty("snow", NullValueHandling = NullValueHandling.Ignore)] public Snow Snow { get; set; }
     }
